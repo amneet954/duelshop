2ee5d800fb271aa67bc5d5d9b1e787fcb930565a
Amneet Sandhu <amneet954@gmail.com>
2022-03-26 02:01:30 -0400

    Added redux,dark mode, and changed font

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -45,7 +45,9 @@ const ProductScreen = () => {
         <Grid item md={3} xs={12}>
           <List>
             <ListItem>
-              <Typography component="h1">Card Name: {product.name}</Typography>
+              <Typography component="h1" variant="h1">
+                {product.name}
+              </Typography>
             </ListItem>
             <ListItem>
               <Typography>Category: {product.category}</Typography>
